8f944c6906795a297d17b868a1339a41dbab2ab8
Marc A. Anderson <marc@marcadam.com>
2016-02-04 22:05:47 -0800

    Add release date and rating to detail view

--- Flicks/Flicks/MovieDetailViewController.swift
+++ Flicks/Flicks/MovieDetailViewController.swift
@@ -14,6 +14,8 @@ class MovieDetailViewController: UIViewController {
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var movieInfoView: UIView!
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
     @IBOutlet weak var overviewLabel: UILabel!
 
     var movie: NSDictionary?
@@ -35,14 +37,28 @@ class MovieDetailViewController: UIViewController {
                 overviewLabel.sizeToFit()
             }
 
+            if let releaseDate = movie["release_date"] as? String {
+                releaseDateLabel.text = releaseDate
+            }
+
+            if let rating = movie["vote_average"] as? Double {
+                ratingLabel.text = "\(rating)"
+            }
+
             let padding = CGFloat(8)
             let movieInfoViewDefaultVisableHeight = CGFloat(80.0)
 
-            let movieInfoViewHeight = titleLabel.bounds.height + overviewLabel.bounds.height + padding * 4
+            let movieInfoViewHeight = titleLabel.bounds.height + overviewLabel.bounds.height + releaseDateLabel.frame.height + padding * 5
             movieInfoView.frame.origin = CGPoint(x: view.frame.origin.x, y: view.frame.height - movieInfoViewDefaultVisableHeight)
             movieInfoView.frame.size = CGSize(width: movieInfoView.frame.width, height: movieInfoViewHeight)
 
-            let overviewOriginY = titleLabel.bounds.height + padding * 2
+            let releaseDateOriginY = titleLabel.bounds.height + padding * 2
+            releaseDateLabel.frame.origin = CGPoint(x: releaseDateLabel.frame.origin.x, y: releaseDateOriginY)
+
+            let ratingOriginY = releaseDateOriginY
+            ratingLabel.frame.origin = CGPoint(x: ratingLabel.frame.origin.x, y: ratingOriginY)
+
+            let overviewOriginY = titleLabel.bounds.height + releaseDateLabel.bounds.height + padding * 3
             overviewLabel.frame.origin = CGPoint(x: overviewLabel.frame.origin.x, y: overviewOriginY)
 
             let contentWidth = scrollView.bounds.width
